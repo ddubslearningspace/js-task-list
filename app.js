dd552d8caad48da3a7aff2d43562121bab38ec04
Daniel Warner <52971033+ddubthedev@users.noreply.github.com>
2020-08-25 17:17:31 -0600

    added delete and filter functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Define UI Vars
 const form = document.querySelector('#task-form');
-const tasklist = document.querySelector('.collection');
+const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
@@ -9,36 +9,74 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 function loadEventListeners() {
-    // Add task event
-    form.addEventListener('submit', addTask);
+	// Add task event
+	form.addEventListener('submit', addTask);
+	// Remove task event
+	taskList.addEventListener('click', removeTask);
+	// Clear tasks event
+	clearBtn.addEventListener('click', clearTasks);
+	// Filter tasks event
+	filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
 function addTask(e) {
-    if(taskInput.value === '') {
-        alert('Add a task');
-    }
+	if (taskInput.value === '') {
+		alert('Add a task');
+	}
 
-    // Create li element
-    const li = document.createElement('li');
-    // Add a class
-    li.className = 'collection-item';
-    // Create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
-    // Create new link elemt
-    const link = document.createElement('a');
-    // Add a class
-    link.className = 'delete-item secondary-content';
-    // Add icon html
-    link.innerHTML = '<i class="fa fa-times"></i>';
-    // Append link to li
-    li.appendChild(link);
+	// Create li element
+	const li = document.createElement('li');
+	// Add a class
+	li.className = 'collection-item';
+	// Create text node and append to li
+	li.appendChild(document.createTextNode(taskInput.value));
+	// Create new link elemt
+	const link = document.createElement('a');
+	// Add a class
+	link.className = 'delete-item secondary-content';
+	// Add icon html
+	link.innerHTML = '<i class="fa fa-times"></i>';
+	// Append link to li
+	li.appendChild(link);
 
-    // Append li to ul
-    tasklist.appendChild(li);
+	// Append li to ul
+	taskList.appendChild(li);
 
-    // Clear input
-    taskInput.value = '';
+	// Clear input
+	taskInput.value = '';
 
-    e.preventDefault();
+	e.preventDefault();
+}
+
+// Remove task
+function removeTask(e) {
+	if (e.target.parentElement.classList.contains('delete-item')) {
+		if (confirm('Are you sure?')) {
+			e.target.parentElement.parentElement.remove();
+		}
+	}
+}
+
+// Clear tasks
+function clearTasks(e) {
+	if (confirm('Are you sure?')) {
+		while (taskList.firstChild) {
+			taskList.removeChild(taskList.firstChild);
+		}
+	}
+}
+
+// Filter tasks
+function filterTasks(e) {
+	const text = e.target.value.toLowerCase();
+	
+	document.querySelectorAll('.collection-item').forEach(function(task) {
+		const item = task.firstChild.textContent;
+		if(item.toLowerCase().indexOf(text) != -1) {
+			task.style.display = "block";
+		} else {
+			task.style.display = "none";
+		}
+	});
 }
