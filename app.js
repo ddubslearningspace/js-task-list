a9b49a486abfa030fbd97d2184e62a6afd1ad0cf
Daniel Warner <52971033+ddubthedev@users.noreply.github.com>
2020-08-25 16:20:23 -0600

    created add task function

--- app.js
+++ app.js
@@ -4,3 +4,41 @@ const tasklist = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+    // Add task event
+    form.addEventListener('submit', addTask);
+}
+
+// Add task
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('Add a task');
+    }
+
+    // Create li element
+    const li = document.createElement('li');
+    // Add a class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link elemt
+    const link = document.createElement('a');
+    // Add a class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-times"></i>';
+    // Append link to li
+    li.appendChild(link);
+
+    // Append li to ul
+    tasklist.appendChild(li);
+
+    // Clear input
+    taskInput.value = '';
+
+    e.preventDefault();
+}
