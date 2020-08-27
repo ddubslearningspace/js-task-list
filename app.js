14f5f1122640f917864868142778ce26f9c7ab83
Daniel Warner <52971033+ddubthedev@users.noreply.github.com>
2020-08-27 15:19:43 -0600

    reformatted  files

--- app.js
+++ app.js
@@ -11,18 +11,22 @@ loadEventListeners();
 function loadEventListeners() {
 	// DOM load event
 	document.addEventListener('DOMContentLoaded', getTasks);
+
 	// Add task event
 	form.addEventListener('submit', addTask);
+
 	// Remove task event
 	taskList.addEventListener('click', removeTask);
+
 	// Clear tasks event
 	clearBtn.addEventListener('click', clearTasks);
+
 	// Filter tasks event
 	filter.addEventListener('keyup', filterTasks);
 }
 
-// Get tasks from localStorage
-function getTasks() {
+// Initialize task list
+function initializeTasks() {
 	let tasks;
 
 	if (localStorage.getItem('tasks') === null) {
@@ -31,19 +35,32 @@ function getTasks() {
 		tasks = JSON.parse(localStorage.getItem('tasks'));
 	}
 
-	tasks.forEach(function(task) {
+	return tasks;
+}
+
+// Get tasks from localStorage
+function getTasks() {
+	let tasks = initializeTasks();
+
+	tasks.forEach(function (task) {
 		// Create li element
 		const li = document.createElement('li');
+
 		// Add a class
 		li.className = 'collection-item';
+
 		// Create text node and append to li
 		li.appendChild(document.createTextNode(task));
+
 		// Create new link elemt
 		const link = document.createElement('a');
+
 		// Add a class
 		link.className = 'delete-item secondary-content';
+
 		// Add icon html
 		link.innerHTML = '<i class="fa fa-times"></i>';
+
 		// Append link to li
 		li.appendChild(link);
 
@@ -60,16 +77,22 @@ function addTask(e) {
 
 	// Create li element
 	const li = document.createElement('li');
+
 	// Add a class
 	li.className = 'collection-item';
+
 	// Create text node and append to li
 	li.appendChild(document.createTextNode(taskInput.value));
+
 	// Create new link elemt
 	const link = document.createElement('a');
+
 	// Add a class
 	link.className = 'delete-item secondary-content';
+
 	// Add icon html
 	link.innerHTML = '<i class="fa fa-times"></i>';
+
 	// Append link to li
 	li.appendChild(link);
 
@@ -87,13 +110,7 @@ function addTask(e) {
 
 // Store task
 function storeTaskInLocalStorage(task) {
-	let tasks;
-
-	if (localStorage.getItem('tasks') === null) {
-		tasks = [];
-	} else {
-		tasks = JSON.parse(localStorage.getItem('tasks'));
-	}
+	let tasks = initializeTasks();
 
 	tasks.push(task);
 
@@ -106,6 +123,7 @@ function removeTask(e) {
 		if (confirm('Are you sure?')) {
 			// remove from DOM
 			e.target.parentElement.parentElement.remove();
+
 			// remove from localStorage
 			removeTaskFromLocalStorage(e.target.parentElement.parentElement);
 		}
@@ -114,16 +132,10 @@ function removeTask(e) {
 
 // Remove from localStorage
 function removeTaskFromLocalStorage(taskItem) {
-	let tasks;
+	let tasks = initializeTasks();
 
-	if (localStorage.getItem('tasks') === null) {
-		tasks = [];
-	} else {
-		tasks = JSON.parse(localStorage.getItem('tasks'));
-	}
-
-	tasks.forEach(function(task, index) {
-		if(taskItem.textContent === task) {
+	tasks.forEach(function (task, index) {
+		if (taskItem.textContent === task) {
 			tasks.splice(index, 1);
 		}
 	});
@@ -153,6 +165,7 @@ function filterTasks(e) {
 
 	document.querySelectorAll('.collection-item').forEach(function (task) {
 		const item = task.firstChild.textContent;
+
 		if (item.toLowerCase().indexOf(text) != -1) {
 			task.style.display = "block";
 		} else {
